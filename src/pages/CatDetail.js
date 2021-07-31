ff8a7fe78caa8c9df8e71511b0a384d9d1799518
John <iamjohnraycollot@gmail.com>
2021-08-01 03:08:25 +0800

    update on cat details

--- src/pages/CatDetail.js
+++ src/pages/CatDetail.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, {useState, useContext, useEffect} from 'react'
 import { Button } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import {CatContext} from '../components/CatContext';
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'https://api.thecatapi.com/v1/',
+  headers: {'x-key-api': '506d311b-c494-4e14-a75c-dffc90d30fd5'}
+});
 
 const CatDetail = () => {
+  const [catSelected, setCatSelected] = useContext(CatContext);
+  const [catData, setCatData] = useState([]);
+  let url = 'images/'+catSelected.id;
+  useEffect( ()=>{
+    api.get(url)
+    .then(res => {
+      setCatData(res.data);
+      console.log()
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }, []);
+
   return (
     <Row>
       <Row>
@@ -13,20 +34,17 @@ const CatDetail = () => {
           <Link to='/'>
             <Button variant="primary">Back</Button>
           </Link>
-          Cat Details
         </Col>
       </Row>
       <Row>
         <Col>
           <Card style={{ width: '50rem' }}> 
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Img variant="top" src={catData.url} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <Card.Title>{catData.breeds!=undefined ? catData.breeds[0].name : 'No breed available.'}</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
+              {catData.breeds!=undefined ? catData.breeds[0].description : 'No description available.'}
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
             </Card.Body>
           </Card>
         </Col>
